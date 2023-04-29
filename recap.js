b86a9da35699339ab2e1a330e88b18a173a721f7
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-29 13:26:57 +0600

    default parameter

--- recap.js
+++ recap.js
@@ -4,3 +4,18 @@ const numbers = [10, 20, 30, 40, 50];
 let salary = 12000;
 salary = 15000;
 
+// 2. default parameter for function
+function calculateSalary(salary, tax = 0.25, bonus = 2000){
+    const remaining = salary - salary * tax;
+    const totalSalary = remaining + bonus; 
+    return totalSalary;
+}
+
+//default parameter for arrow function
+const income = (s, t, b = 1000) => {
+    const remainingSalary = s - s * t;
+    const total = remainingSalary + b;
+    return total;
+}
+const myIncome = income(10000, 0);
+console.log(myIncome);
