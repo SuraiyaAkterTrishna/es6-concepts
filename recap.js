82ca05d106e17f2ec03ed721bb6ed6664bfe986f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-04-29 13:29:52 +0600

    template string

--- recap.js
+++ recap.js
@@ -19,3 +19,13 @@ const income = (s, t, b = 1000) => {
 }
 const myIncome = income(10000, 0);
 console.log(myIncome);
+
+// 3. template string
+const elementHTML = `
+    <div>
+        <h1>Name: </h1>
+        <p>Address: </p>
+        <p>Salary: ${calculateSalary(5000, 0, 1000)}</p>
+        <p>Others: ${numbers[2]}</p>
+    </div>
+`;
