1ac7fd821501f21ce0967a2b00f85bbd151db4ad
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-01 22:25:45 +0600

    problem 2 done

---
+++ problem2.js
@@ -0,0 +1,8 @@
+/* You are given an array say: [33, 50, 79, 78, 90, 101, 30 ]. Now
+return/get all the elements which are divisible by 10 using
+array.filter() method. */
+
+const numbers = [33, 50, 79, 78, 90, 101, 30 ];
+const tens = numbers.filter(number => number % 10 === 0);
+console.log(tens);
+
