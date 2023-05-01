6e6b4479b20da2b8f49ea7bc5391fe684d9d007c
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-01 10:24:31 +0600

    more map

---
+++ more-map.js
@@ -0,0 +1,5 @@
+const numbers = [12, 24, 53, 64, 54, 89];
+const half = numbers.map(number => number / 2);
+console.log(half);
+const thirds = numbers.map(number => number / 3);
+console.log(thirds);
