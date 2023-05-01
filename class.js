30552ba6bdf5422d8490b5e1f0043bd8bfa89e91
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-01 17:42:10 +0600

    class, object, method, constructor

--- class.js
+++ class.js
@@ -1,8 +1,26 @@
+// class declaration
 class Instructor{
     name;
     designation = 'Web Instructor';
     team = 'Web Team';
     location;
+    //constructor
+    constructor(name, location){
+        this.name = name;
+        this.location = location;
+    }
+    //methods
+    supportSession(time){
+        console.log(`Start the support session at: ${time}`);
+    };
+    createQuiz(module){
+        console.log(`Create quiz for the module: ${module}`);
+    }
 };
-const aamir = new Instructor();
+const aamir = new Instructor('Aamir', 'Mumbai'); //create object
 console.log(aamir);
+aamir.supportSession('9.00 am');
+aamir.createQuiz(61);
+
+const solaiman = new Instructor('Solaiman', 'Dhaka');
+console.log(solaiman);
