c3d5374ad6d90dfcdbe1ea96ba4e4c939f2c894a
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-01 11:58:32 +0600

    filter

--- filter.js
+++ filter.js
@@ -5,3 +5,15 @@ const tiny = numbers.filter(number => number < 10);
 console.log(tiny);
 const even = numbers.filter(number => number % 2 === 0);
 console.log(even);
+
+const products = [
+    {id: 1, name: 'laptop', price: 45000},
+    {id: 1, name: 'mobile', price: 80000},
+    {id: 1, name: 'watch', price: 35000},
+    {id: 1, name: 'tablet', price: 23000},
+];
+
+const expensive = products.filter(product => product.price > 40000);
+console.log(expensive);
+const cheapest = products.filter(product => product.price < 40000);
+console.log(cheapest);
